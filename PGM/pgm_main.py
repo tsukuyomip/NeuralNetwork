a683b1cf1e99a1227de82eeabbd0baf5761f6dc1
tsukuyomip <ab0312uo@gmail.com>
2014-07-15 01:48:55 +0900

    pgm_main.py にコメントを追加

--- PGM/pgm_main.py
+++ PGM/pgm_main.py
@@ -5,40 +5,43 @@ import PGM
 
 if __name__ == "__main__":
     rng = np.random.RandomState(2294322)
-    #rng = None
+    n_loop_theta = 100  # thetaの増分は(1.0/n_loop_theta)．プロット点に対応．
     pgm = PGM.PGM(n_generate = 100000, sigma = 1.0, rng = rng)
 
-    #for i in range(pgm.n_generate):
-    #    print >> sys.stderr,  pgm.x[i]
-    #    print >> sys.stderr,  pgm.y[i]
-
+    # 目的とするパターンが何個出てきたか
     print >> sys.stderr,  "n_appear =", pgm.n_appear
-    n_loop_theta = 100
+
+
     for t in range(0, n_loop_theta + 1):
         theta = float(t) / n_loop_theta
 
         n_FP = 0
         n_CD = 0
+
+        # すべてのパターンx[i]について，sを計算
         for i in range(pgm.n_generate):
-            #s = pgm.compute_S_mean(index = i)
-            s = rng.rand()
+            s = pgm.compute_S_mean(index = i)
+            #s = rng.rand()
             if s > theta:
                 z = 1
             else:
                 z = 0
-        
+
+            # x[i]をTrueだと判断した場合にCDかFPをカウント
             if z == 1:
                 if pgm.x[i] == pgm.state_x[pgm.check_index]:
                     n_CD += 1
                 else:
                     n_FP += 1
-        print >> sys.stderr,  "n_FP =", n_CD
-        print >> sys.stderr,  "n_CD =", n_FP
+
+        print >> sys.stderr,  "theta =", theta
+        print >> sys.stderr,  "\tn_FP =", n_CD
+        print >> sys.stderr,  "\tn_CD =", n_FP
 
         fpr = float(n_FP)/(pgm.n_generate - pgm.n_appear)
         cdr = float(n_CD)/pgm.n_appear
 
-        print >> sys.stderr,  "FPR =", fpr
-        print >> sys.stderr,  "CDR =", cdr
-
+        print >> sys.stderr,  "\tFPR =", fpr
+        print >> sys.stderr,  "\tCDR =", cdr
         print "%lf %lf" % (fpr, cdr)
+        print >> sys.stderr,  ""
